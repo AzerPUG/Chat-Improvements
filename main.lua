2286216cb5cd14ab0c450213d1e97611b8c35e40
Arco Dielhof <arcodielhof@hotmail.com>
2020-10-26 11:46:15 +0100

    Auto-Upload Test1.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AGU = ...
 
-local AZPGUChattyThingsVersion = 3
+local AZPGUChattyThingsVersion = 4
 local dash = " - "
 local name = "GameUtility" .. dash .. "ChattyThings"
 local nameFull = ("AzerPUG " .. name)
