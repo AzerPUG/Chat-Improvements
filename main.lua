69f35421195772ee0b868cc71a7a1925f55ff937
Arco Dielhof <arcodielhof@hotmail.com>
2021-01-05 22:08:51 +0100

    Added Twilight Community boosting spam to block. Fixed a version issue that (potentially) caused problems for some people?

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AGU = ...
 
-local AZPGUChattyThingsVersion = 10
+local AZPGUChattyThingsVersion = 12
 local dash = " - "
 local name = "GameUtility" .. dash .. "ChattyThings"
 local nameFull = ("AzerPUG " .. name)
