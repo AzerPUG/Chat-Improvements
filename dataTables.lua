0e04b7e3ee26ec64fc1b6902f2ec7206db1b4c33
Arco Dielhof <arcodielhof@hotmail.com>
2021-02-07 23:09:51 +0100

    Added another boost spam community.

--- dataTables.lua
+++ dataTables.lua
@@ -6,5 +6,5 @@ AGU.KeyPhrases =
         "Boost", "Boosting",
         "Selling Mythic", "Selling Heroic", "Selling Torghast", "Selling Castle", "Selling Keystone", "Selling M+", "Selling Sire",
         "WTS Mythic", "WTS Heroic", "WTS Torghast", "WTS Castle", "WTS Keystone", "WTS M+", "WTS Sire",
-        "Gallywix", "Nova", "Icecrown", "Sylvanas", "RCU", "Oblivion", "Hydra", "Krakenboost", "SBS", "OOU", "Skycoach", "Twilight", "Exsorsus",
+        "Gallywix", "Nova", "Icecrown", "Sylvanas", "RCU", "Oblivion", "Hydra", "Krakenboost", "SBS", "OOU", "Skycoach", "Twilight", "Exsorsus", "Titan",
 }
--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AGU = ...
 
-local AZPGUChattyThingsVersion = 16
+local AZPGUChattyThingsVersion = 17
 local dash = " - "
 local name = "GameUtility" .. dash .. "ChattyThings"
 local nameFull = ("AzerPUG " .. name)
