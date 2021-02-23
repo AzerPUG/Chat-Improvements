a31bafc199b176d8704c2a6581c1a44f8bf3ff9f
Arco Dielhof <arcodielhof@hotmail.com>
2021-02-23 16:09:20 +0100

    Added "Selling Denathrius" and "WTS Denathrius" as filtered keyphrases.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AGU = ...
 
-local AZPGUChattyThingsVersion = 18
+local AZPGUChattyThingsVersion = 19
 local dash = " - "
 local name = "GameUtility" .. dash .. "ChattyThings"
 local nameFull = ("AzerPUG " .. name)
