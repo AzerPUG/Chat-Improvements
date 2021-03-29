21b105a2b10db358a5fc83365f6abbbe1d8d7ce7
Arco Dielhof <arcodielhof@hotmail.com>
2021-03-29 13:05:45 +0200

    Added "WTS Arena" and "Selling Arena" as requested.

--- dataTables.lua
+++ dataTables.lua
@@ -4,7 +4,7 @@ _G[GlobalAddonName] = AGU
 AGU.KeyPhrases =
 {
         "Boost", "Boosting",
-        "Selling Mythic", "Selling Heroic", "Selling Torghast", "Selling Castle", "Selling Keystone", "Selling M+", "Selling Sire", "Selling Denathrius", "Selling Nathria",
-        "WTS Mythic", "WTS Heroic", "WTS Torghast", "WTS Castle", "WTS Keystone", "WTS M+", "WTS Sire", "WTS Denathrius", "WTS Nathria",
+        "Selling Mythic", "Selling Heroic", "Selling Torghast", "Selling Castle", "Selling Keystone", "Selling M+", "Selling Sire", "Selling Denathrius", "Selling Nathria", "Selling Arena",
+        "WTS Mythic", "WTS Heroic", "WTS Torghast", "WTS Castle", "WTS Keystone", "WTS M+", "WTS Sire", "WTS Denathrius", "WTS Nathria", "WTS Arena",
         "Gallywix", "Nova", "Icecrown", "Sylvanas", "RCU", "Oblivion", "Hydra", "Krakenboost", "SBS", "OOU", "Skycoach", "Twilight", "Exsorsus", "Titan", "NBC", "Triple DR",
 }
--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AGU = ...
 
-local AZPGUChattyThingsVersion = 22
+local AZPGUChattyThingsVersion = 23
 local dash = " - "
 local name = "GameUtility" .. dash .. "ChattyThings"
 local nameFull = ("AzerPUG " .. name)
